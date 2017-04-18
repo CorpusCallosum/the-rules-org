9c17c1e5daf440532f3b2ebed15a32eff7d0afb4
Jack Kalish <jykalish@gmail.com>
2017-04-18 16:16:17 -0400

    angular 2 tutorial progresses

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,19 +2,34 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { RouterModule }   from '@angular/router';
 
-import { AppComponent } from './app.component';
+import{AppRoutingModule}  from './app-routing.module'
 
+//my custom components and services imported here
+import { AppComponent }        from './app.component';
+import { HeroDetailComponent } from './hero-detail.component';
+import { HeroesComponent }     from './heroes.component';
+import { HeroService }         from './hero.service';
+import { DashboardComponent }  from './dashboard.component';
+
+
+//
 @NgModule({
   declarations: [
-    AppComponent
+  //and declared here:
+    AppComponent,
+    DashboardComponent,
+    HeroDetailComponent,
+    HeroesComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AppRoutingModule
   ],
-  providers: [],
+  providers: [HeroService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
