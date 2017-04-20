ada409ce8b26e56457b6a8a74c776b23f4c2fefe
Jack Kalish <jykalish@gmail.com>
2017-04-20 18:29:54 -0400

    cleanup and dust build again

---
+++ src/app/hero.service.js
@@ -0,0 +1,33 @@
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+import { Injectable } from '@angular/core'; //wahtever that means
+import { HEROES } from './mock-heroes'; //import mock data
+var HeroService = (function () {
+    function HeroService() {
+    }
+    //this just returns the hero array mock data, as a promise
+    HeroService.prototype.getHeroes = function () {
+        return Promise.resolve(HEROES);
+    };
+    HeroService.prototype.getHeroesSlowly = function () {
+        var _this = this;
+        return new Promise(function (resolve) {
+            // Simulate server latency with 2 second delay
+            setTimeout(function () { return resolve(_this.getHeroes()); }, 2000);
+        });
+    };
+    HeroService.prototype.getHero = function (id) {
+        return this.getHeroes()
+            .then(function (heroes) { return heroes.find(function (hero) { return hero.id === id; }); });
+    };
+    return HeroService;
+}());
+HeroService = __decorate([
+    Injectable()
+], HeroService);
+export { HeroService };
+//# sourceMappingURL=hero.service.js.map
