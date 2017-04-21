2a9e2eacde5de371f3dfc47153092d812fe7cb3d
Jack Kalish <jykalish@gmail.com>
2017-04-21 16:01:26 -0400

    added FAQ page with collapsible panels

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,11 +4,14 @@ import { RouterModule, Routes } from '@angular/router';
 //ROUTES
 import { HomePageComponent }    from './home-page.component';
 import { AboutPageComponent }   from './about-page.component';
+import { FaqPageComponent }     from './faq-page/faq-page.component';
+
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home',  component: HomePageComponent },
-  { path: 'about',  component: AboutPageComponent }
+  { path: 'about',  component: AboutPageComponent },
+  { path: 'faq',  component: FaqPageComponent }
 ];
 
 @NgModule({
