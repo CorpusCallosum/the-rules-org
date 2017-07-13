a6352c49c62afe5ba844c8006293bf5a5df10834
Jack Kalish <jack.kalish@rokkan.com>
2017-07-13 18:32:02 -0400

    added video scaling to resolve:
    " On some versions - “our story” is not seen above the fold of the page - could you make sure this first text title is always seen above the fold."
    
    scaling hero video using "cover" method

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, ChangeDetectorRef } from '@angular/core';
+declare var jQuery:any;
 
 @Component({
   selector: 'my-app',
@@ -32,6 +33,13 @@ export class HomePageComponent implements OnInit {
     this.videoObject.addEventListener('ended',this.onVideoEnded.bind(this),false);
     this.videoObject.addEventListener('loadeddata',this.onVideoLoaded.bind(this),false);
     //this.videoTitle = this.videos[this.activeVideoId].title;
+
+    this.vid_w_orig = parseInt(jQuery('video').attr('width'));
+    this.vid_h_orig = parseInt(jQuery('video').attr('height'));
+    jQuery('#debug').append("<p>DOM loaded</p>");
+
+    jQuery(window).resize(this.resizeToCover);
+    jQuery(window).trigger('resize');
    }
 
    onVideoLoaded():void{
@@ -52,5 +60,32 @@ export class HomePageComponent implements OnInit {
       this.hideVideo = true;
       this.ref.detectChanges();
    }
+
+   //video resizing stuff...
+   min_w = 300; // minimum video width allowed
+   vid_w_orig;  // original video dimensions
+   vid_h_orig;
+   resizeToCover():void {
+     console.log("resizeToCover");
+    var h = .8; //video heigh as percentage of window height
+    // set the video viewport to the window size
+    jQuery('#video-viewport').width(jQuery(window).width());
+    jQuery('#video-viewport').height(jQuery(window).height()*h);
+
+    // use largest scale factor of horizontal/vertical
+    var scale_h = jQuery(window).width() / this.vid_w_orig;
+    var scale_v = jQuery(window).height() / this.vid_h_orig;
+    var scale = scale_h > scale_v ? scale_h : scale_v;
+
+    // don't allow scaled width < minimum video width
+    if (scale * this.vid_w_orig < this.min_w) {scale = this.min_w / this.vid_w_orig;};
+
+    // now scale the video
+    jQuery('#hero-video').width(scale * this.vid_w_orig);
+    jQuery('#hero-video').height(scale * this.vid_h_orig);
+    // and center it by scrolling the video viewport
+    jQuery('#video-viewport').scrollLeft((jQuery('#hero-video').width() - jQuery(window).width()) / 2);
+    jQuery('#video-viewport').scrollTop((jQuery('#hero-video').height() - jQuery(window).height()*h) / 2);
+  };
 
 }
