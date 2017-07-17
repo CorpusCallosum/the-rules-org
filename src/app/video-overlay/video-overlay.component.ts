775143bc667572e15ad0769a8fe80acb84e5f951
Jack Kalish <jack.kalish@rokkan.com>
2017-07-17 15:44:15 -0400

    video overlay loading on video clicky

--- src/app/video-overlay/video-overlay.component.ts
+++ src/app/video-overlay/video-overlay.component.ts
@@ -4,19 +4,33 @@ import {DomSanitizer,SafeResourceUrl,} from '@angular/platform-browser';
 @Component({
   selector: 'app-video-overlay',
   templateUrl: './video-overlay.component.html',
-  styleUrls: ['./video-overlay.component.css']
+  styleUrls: ['./video-overlay.component.scss']
 })
 export class VideoOverlayComponent implements OnInit {
 
   @Input() ytid:string;
   url: SafeResourceUrl;
 
+  isOpen:Boolean = false;
+  debug:Boolean = true;
+
   constructor(public sanitizer:DomSanitizer) { }
 
   ngOnInit() {
     //concat the id into the youtube src url
     var ytUrl = "https://www.youtube.com/embed/"+this.ytid+"?rel=0&amp;showinfo=0?ecver=1";
-    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(ytUrl);      
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(ytUrl);
+  }
+
+  launch(){
+    if(this.debug)
+      console.log('launch video overlay');
+
+    this.isOpen = true;
+  }
+
+  close(){
+    this.isOpen = false;
   }
 
 }
