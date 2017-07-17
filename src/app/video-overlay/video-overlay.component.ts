efe77335795755c4d56ac8421a74bde81850663b
Jack Kalish <jack.kalish@rokkan.com>
2017-07-17 17:55:48 -0400

    video plying and stopping in overlay, and added a close btn too!

--- src/app/video-overlay/video-overlay.component.ts
+++ src/app/video-overlay/video-overlay.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input, ChangeDetectorRef } from '@angular/core';
 import {DomSanitizer,SafeResourceUrl,} from '@angular/platform-browser';
 
 @Component({
@@ -13,24 +13,63 @@ export class VideoOverlayComponent implements OnInit {
 
   isOpen:Boolean = false;
   debug:Boolean = true;
+  monitor;
 
-  constructor(public sanitizer:DomSanitizer) { }
+
+  constructor(public sanitizer:DomSanitizer, private ref: ChangeDetectorRef) { }
 
   ngOnInit() {
     //concat the id into the youtube src url
     var ytUrl = "https://www.youtube.com/embed/"+this.ytid+"?rel=0&amp;showinfo=0?ecver=1";
     this.url = this.sanitizer.bypassSecurityTrustResourceUrl(ytUrl);
+
+    //init the yt player...
+    /*var player;
+      function onYouTubeIframeAPIReady() {
+        player = new YT.Player('player', {
+          height: '390',
+          width: '640',
+          videoId: 'M7lc1UVf-VE',
+          events: {
+            'onReady': onPlayerReady,
+            'onStateChange': onPlayerStateChange
+          }
+        });
+      }*/
+
+      this.monitor = setInterval(this.checkfocus.bind(this), 100);
+
+  }
+
+  checkfocus(){
+        var elem = document.activeElement;
+        if(elem && elem.tagName == 'IFRAME'){
+            clearInterval(this.monitor);
+            //alert('clicked!');
+            this.launch();
+            this.isOpen = true;
+        }
   }
 
   launch(){
     if(this.debug)
       console.log('launch video overlay');
 
-    this.isOpen = true;
+   // this.isOpen = true;
   }
 
   close(){
     this.isOpen = false;
+    this.monitor = setInterval(this.checkfocus.bind(this), 100);
+
+    //stop the video
+    this.url = "";
+    this.ref.markForCheck();
+
+    var ytUrl = "https://www.youtube.com/embed/"+this.ytid+"?rel=0&amp;showinfo=0?ecver=1";
+    this.url = this.sanitizer.bypassSecurityTrustResourceUrl(ytUrl);
+
+
   }
 
 }
