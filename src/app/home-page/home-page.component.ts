8589573b385f472ddcc9420e6c4b48da7465d1b5
Jack Kalish <jykalish@gmail.com>
2017-08-25 19:52:19 -0400

    added image slideshow for mobile

--- src/app/home-page/home-page.component.ts
+++ src/app/home-page/home-page.component.ts
@@ -13,12 +13,12 @@ export class HomePageComponent implements OnInit {
     {
       "src":"assets/videos/hero/CHANGE_THE_RULES.m4v",
       "title":"Brooklyn offal retro drinking vinegar officia mixtape meggings literally bicycle rights. Duis before they sold out ugh, kitsch wayfarers shaman affogato twee synth chia laboris subway tile post-ironic air plant salvia. 3 wolf moon jean shorts pok pok man braid la croix PBR&B artisan deserunt nulla asymmetrical nisi messenger bag air plant snackwave keffiyeh.",
-      "image":"assets/images/standing-rock.png"
+      "image":"assets/images/slideshow/change-the-rules.jpg"
     },
     {
       "src":"assets/videos/hero/CULTURE_HACK_LAB.m4v",
       "title":"The CHL is the space where we work to better understand what stories are shaping culture. We seek to answer questions such as: what are the stories we believe and share about how the world works? How do these stories help sustain the way human beings or the environment are defined by the economy? The CHL test the ways of effectively changing those stories over time, so as to change our culture.",
-      "image":"assets/images/standing-rock.png"
+      "image":"assets/images/slideshow/culture-hack-lab.jpg"
     }
   ];
 
@@ -46,7 +46,7 @@ export class HomePageComponent implements OnInit {
     if (typeof window.orientation !== 'undefined') {
       //mobile
       ////add timer for triggering slideshow here...
-      
+
     }
     else{
       //desktop
